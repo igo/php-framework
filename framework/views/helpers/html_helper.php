17e7215882092779cec9a7ff20b24750bfbf818b
igo <igo@inspired.sk>
2009-02-21 12:28:07 +0100

    Added css helper

--- framework/views/helpers/html_helper.php
+++ framework/views/helpers/html_helper.php
@@ -1,15 +1,19 @@
 <?php
 
 class Framework_Views_Helpers_HtmlHelper extends Framework_Views_Helpers_Helper {
-	
+
 	public function escape($s) {
 		return htmlspecialchars($s);
 	}
-	
+
 	public function link($title, $url) {
 		echo '<a href="' . $this->url($url) . '">' . $this->escape($title) . '</a>';
 	}
-	
+
+	public function css($url) {
+		echo '<link rel="stylesheet" type="text/css" href="' . $this->url($url) . '" />';
+	}
+
 }
 
 ?>
