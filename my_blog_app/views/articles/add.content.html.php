8cf5a425a8c5b7f9bd25817437f09e289d19da28
Igor Urminček <igo@inspired.sk>
2009-04-10 19:50:31 +0200

    Added missing files for adding

---
+++ my_blog_app/views/articles/add.content.html.php
@@ -0,0 +1,12 @@
+<form method="post">
+<label for="title">Title</label>
+<input type="text" name="article[title]" id="title" />
+
+<label for="excerpt">Excerpt</label>
+<textarea name="article[excerpt]" id="excerpt"></textarea>
+
+<label for="body">Body</label>
+<textarea name="article[body]" id="body"></textarea>
+
+<input  type="submit" value="Add" />
+</form>
